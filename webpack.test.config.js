475c169ccc090f68d151512cc315dffe5ae4c790
khho <hokwanhung.2019@gmail.com>
2024-01-06 15:49:42 +0800

    fix: resolve conflicts between package.json, webpack.test.config.js and .babelrc with newest babel7 (all tests could run without unexpected errors)

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -1,18 +1,45 @@
 module.exports = {
   // mode: 'development',
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.wasm']
+  },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            ['es2015', {modules: false}],
-            'react',
-            'stage-2'
-          ]
-        }
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { modules: false }],
+              "@babel/preset-react"
+            ],
+            plugins: [
+              "@babel/plugin-syntax-dynamic-import",
+              "@babel/plugin-syntax-import-meta",
+              "@babel/plugin-proposal-class-properties",
+              "@babel/plugin-proposal-json-strings",
+              [
+                "@babel/plugin-proposal-decorators",
+                {
+                  "legacy": true
+                }
+              ],
+              "@babel/plugin-proposal-function-sent",
+              "@babel/plugin-proposal-export-namespace-from",
+              "@babel/plugin-proposal-numeric-separator",
+              "@babel/plugin-proposal-throw-expressions"
+            ]
+          }
+        }]
+        // options: {
+        //   presets: [
+        //     ['es2015', {modules: false}],
+        //     'react',
+        //     'stage-2'
+        //   ]
+        // }
       }
     ]
   }
